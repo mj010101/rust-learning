88a397afcefbc72ed130cdb2fbab26bfc5a311f7
mj010101 <minjun4799@gmail.com>
2025-02-27 16:05:25 +0900

    simple test code for running a blockchain

--- basic-block/src/main.rs
+++ basic-block/src/main.rs
@@ -1,6 +1,18 @@
 pub mod block; 
 pub mod blockchain;
 
+use hex;
+
 fn main() {
-    println!("Hello, world!");
+    let mut bc = blockchain::Blockchain::new();
+
+    bc.add_block("Send 1 Hype to Alice");
+    bc.add_block("Send 2 more HYPE to Alice");
+
+    for block in bc.blocks() {
+        println!("Prev. hash: {:?}", hex::encode(&block.prev_block_hash()));
+        println!("Data: {}", String::from_utf8(block.data().to_vec()).unwrap());
+        println!("Hash: {:?}", hex::encode(&block.hash()));
+        println!();
+    }
 }
