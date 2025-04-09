2916078308bbbf0a41d26a691a112463fbeef08d
mj010101 <minjun4799@gmail.com>
2025-04-09 14:01:00 +0900

    testing types

---
+++ testings/target/debug/deps/testings-8a4b9b7254da1634.d
@@ -0,0 +1,5 @@
+/Users/minjun/Desktop/Rust learning/testings/target/debug/deps/testings-8a4b9b7254da1634: src/main.rs
+
+/Users/minjun/Desktop/Rust learning/testings/target/debug/deps/testings-8a4b9b7254da1634.d: src/main.rs
+
+src/main.rs:
---
+++ testings/target/debug/testings.d
@@ -0,0 +1,1 @@
+/Users/minjun/Desktop/Rust\ learning/testings/target/debug/testings: /Users/minjun/Desktop/Rust\ learning/testings/src/main.rs
